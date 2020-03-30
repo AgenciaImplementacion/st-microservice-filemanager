470f2df3a4846881c00244a1c21e55dd2b070556
Felipe Cano <felipecanol@gmail.com>
2020-03-30 11:29:05 -0400

    feat

--- src/main/java/com/ai/st/microservice/filemanager/dto/rabbitmq/UploadFileMessageDto.java
+++ src/main/java/com/ai/st/microservice/filemanager/dto/rabbitmq/UploadFileMessageDto.java
@@ -10,16 +10,18 @@ public class UploadFileMessageDto implements Serializable {
 	private String namespace;
 	private String driver;
 	private byte[] file;
+	private String filenameZip;
 	
 	public UploadFileMessageDto() {
 
 	}
 
-	public UploadFileMessageDto(String filename, String namespace, String driver, byte[] file) {
+	public UploadFileMessageDto(String filename, String namespace, String driver, byte[] file, String filenameZip) {
 		this.filename = filename;
 		this.namespace = namespace;
 		this.driver = driver;
 		this.file = file;
+		this.filenameZip = filenameZip;
 	}
 
 	public String getFilename() {
@@ -52,6 +54,14 @@ public class UploadFileMessageDto implements Serializable {
 
 	public void setFile(byte[] file) {
 		this.file = file;
+	}
+
+	public String getFilenameZip() {
+		return filenameZip;
+	}
+
+	public void setFilenameZip(String filenameZip) {
+		this.filenameZip = filenameZip;
 	}
 
 }
